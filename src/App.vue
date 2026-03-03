0fe80aaf6d7609ab5e4f0a855ca82474fd69e490
cgil <lao.tseu.is.alive@gmail.com>
2026-03-03 14:34:44 +0100

    adding collide evt

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,12 @@ import World from './components/World.vue'
     </div>
   </header>
 
-  <main>
-  </main>
+  <main></main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 @media (min-width: 1024px) {
--- src/components/World.vue
+++ src/components/World.vue
@@ -1,5 +1,4 @@
-<style scoped>
-</style>
+<style scoped></style>
 
 <template>
   <div>
@@ -7,10 +6,9 @@
   </div>
 </template>
 
-
 <script setup lang="ts">
-import kaplay from "kaplay";
-import {onMounted, ref, useTemplateRef} from "vue";
+import kaplay from 'kaplay'
+import { onMounted, ref, useTemplateRef } from 'vue'
 
 const myDrawRef = useTemplateRef('myDraw')
 const totalWidth = ref(800)
@@ -22,11 +20,10 @@ defineProps<{
   msg: string
 }>()
 
-
 onMounted(() => {
   console.log(`the component is now mounted.`)
-  const ctx =kaplay({
-    background: [135, 206,235],
+  const ctx = kaplay({
+    background: [135, 206, 235],
     width: totalWidth.value,
     height: totalHeight.value,
     scale: 1,
@@ -34,8 +31,8 @@ onMounted(() => {
     debug: true,
   })
   // loading sprites
-  ctx.loadSprite("tux", "tux_idle.png")
-  ctx.loadSprite("cool_cat", "cat_sprites.png", {
+  ctx.loadSprite('tux', 'tux_idle.png')
+  ctx.loadSprite('cool_cat', 'cat_sprites.png', {
     sliceX: 8,
     sliceY: 5,
     anims: {
@@ -64,7 +61,7 @@ onMounted(() => {
       hello: {
         from: 24,
         to: 31,
-        loop: true,
+        loop: false,
         speed: 8,
       },
       // Row 5: Happy (frames 32 to 35)
@@ -75,52 +72,60 @@ onMounted(() => {
         speed: 4, // Very slow droop
       },
     },
-  });
-  ctx.scene('world', ()=>{
+  })
+  ctx.scene('world', () => {
     const floor = ctx.add([
       ctx.rect(totalWidth.value, wallSize.value, { radius: 2 }), // Carré rouge
-      ctx.pos(0, totalHeight.value-wallSize.value),
+      ctx.pos(0, totalHeight.value - wallSize.value),
       ctx.area(),
-      ctx.body({ isStatic: true}),
+      ctx.body({ isStatic: true }),
       ctx.color(10, 250, 0),
-    ]);
+    ])
     ctx.add([
       ctx.rect(32, wallSize.value, { radius: 2 }), // small red wall
-      ctx.pos(200, totalHeight.value-2*wallSize.value),
+      ctx.pos(200, totalHeight.value - 2 * wallSize.value),
       ctx.area(),
-      ctx.body({ isStatic: true}),
+      ctx.body({ isStatic: true }),
       ctx.color(181, 0, 0),
-    ]);
-
+    ])
 
     const player = ctx.add([
-      ctx.sprite("tux"),
+      ctx.sprite('tux'),
       ctx.pos(80, 240),
       ctx.area(),
       ctx.body(), // Physique auto
-    ]);
+    ])
 
     const cool_cat = ctx.add([
-      ctx.sprite("cool_cat"),
+      'cool_cat',  // adding a tag
+      ctx.sprite('cool_cat'),
       ctx.pos(ctx.center()),
       ctx.scale(0.25),
       ctx.area(),
       ctx.body(), // Physique auto
-    ]);
-    cool_cat.play("idle")
+    ])
+    cool_cat.play('idle')
 
-    ctx.setGravity(160);
-    ctx.onKeyPress(["up", "space"], () => player.jump(120));
-    ctx.onKeyPress("left", () => player.move(-speed,0));
-    ctx.onKeyPress("right", () => player.move(speed,0));
-
+    ctx.setGravity(160)
+    ctx.onKeyPress(['up', 'space'], () => player.jump(120))
+    ctx.onKeyDown('left', () => {
+      player.move(-speed, 0)
+      player.flipX = true
+      cool_cat.flipX = false
+    })
+    ctx.onKeyDown('right', () => {
+      player.move(speed, 0)
+      player.flipX = false
+      cool_cat.flipX = true
+    })
 
     // Collision
-    player.onCollide("enemy", () => {
-      ctx.shake(10);
-      player.destroy();
-    });
-
+    player.onCollide('cool_cat', () => {
+      ctx.shake(10)
+      cool_cat.flipX = true
+      cool_cat.play('hello')
+      player.destroy()
+    })
   })
 
   ctx.go('world')
